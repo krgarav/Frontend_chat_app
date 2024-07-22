619814ec1603993f625616f5733bae303a5cd96d
krgarav <krgarav@gmail.com>
2024-07-22 11:45:47 +0530

    Calling page added

---
+++ src/Component/CallingPage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { MdCallEnd } from "react-icons/md";
+const CallingPage = () => {
+  return (
+    <>
+      <div>CallingPage</div>
+      <div>
+        <MdCallEnd />
+      </div>
+    </>
+  );
+};
+
+export default CallingPage;
